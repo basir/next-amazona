ef20b4141a6ee46ff7f4c604779f8ef344d72596
Basir <basir.jafarzadeh@gmail.com>
2022-01-16 12:38:08 +0330

    Prevent Seeding Data In Production

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -8,13 +8,13 @@ const handler = nc();
 
 handler.get(async (req, res) => {
   return res.send({ message: 'already seeded' });
-  await db.connect();
-  await User.deleteMany();
-  await User.insertMany(data.users);
-  await Product.deleteMany();
-  await Product.insertMany(data.products);
-  await db.disconnect();
-  res.send({ message: 'seeded successfully' });
+  // await db.connect();
+  // await User.deleteMany();
+  // await User.insertMany(data.users);
+  // await Product.deleteMany();
+  // await Product.insertMany(data.products);
+  // await db.disconnect();
+  // res.send({ message: 'seeded successfully' });
 });
 
 export default handler;
